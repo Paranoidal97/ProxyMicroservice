3f87fdc661f25d4564d9875e79b61ca87547e120
paranoidal97 <LukaszRams1997@gmail.com>
2024-01-03 12:26:00 +0100

    initial commit

---
+++ src/main/java/com/paranoidal97/ProxyMicroservice/ProxyMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.paranoidal97.ProxyMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paranoidal97/ProxyMicroservice/ProxyMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paranoidal97.ProxyMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
